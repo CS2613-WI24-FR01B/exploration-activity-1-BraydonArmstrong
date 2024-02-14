8a61b7dfd1cf4da9556af88a17d0edd5a4ba1abb
BraydonArmstrong <armstrongbraydon203@gmail.com>
2024-02-13 23:51:22 -0400

    Fixed where the return statement was in editor.py

--- editor.py
+++ editor.py
@@ -2,13 +2,15 @@ import csv
 
 def convert(csvin):
     try:
+        open(csvin + '.csv', newline='')
+        o = open(csvin + '.txt', "w")
         with open(csvin + '.csv', newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
-            o = open(csvin + '.txt', "w")
+            
             for i,row in enumerate(reader):
                 for j,column in enumerate(row):
                     if(column != ""):
                         o.write(column + " " + str(j * 50) + " " + str(500 + (13 - i) * -50) + "\n")
-                        return "Completed succesfully"
+            return "Completed succesfully"
     except:
         return "Error Occured, Is there a CSV with that name in the directory?"
